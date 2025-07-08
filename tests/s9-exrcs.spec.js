a98dbc24608e7da563c413819a594fd505476af7
pazmiguela <mpmrhm2018@gmail.com>
2025-07-08 18:43:04 +0800

    S9 Exercise Done

--- tests/s9-exrcs.spec.js
+++ tests/s9-exrcs.spec.js
@@ -8,12 +8,14 @@ test.describe('User Login Scenarios', () => {
   });
 
   test('Successful login with valid credentials', async ({ page }) => {
-    await page.locator('input[name="john"]').fill('validUser');
-    await page.locator('input[name="demo"]').fill('validPass');
+    await page.locator('input[name="username"]').fill('john');
+    await page.locator('input[name="password"]').fill('demo');
     await page.getByRole('button', { name: 'Log In' }).click();
 
     // Assert that the user is redirected to the account overview page
-    await expect(page.getByText('Accounts Overview')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Accounts Overview' })).toBeVisible();
+    await page.screenshot({ path: 'screenshots/full-page.png', fullPage: true });
+
   });
 
   test('Unsuccessful login with invalid credentials', async ({ page }) => {
@@ -22,6 +24,8 @@ test.describe('User Login Scenarios', () => {
     await page.getByRole('button', { name: 'Log In' }).click();
 
     // Assert that an error message is shown
-    await expect(page.locator('#rightPanel .error')).toContainText('The username and password could not be verified.');
+    await expect(page.locator('#rightPanel .error')).toContainText('An internal error has occurred and has been logged.');
+    await page.screenshot({ path: 'screenshots/full-page.png', fullPage: true });
+
   });
 });
