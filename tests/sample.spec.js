81346a4d0f3fc3b2cc070615c8b3448ff97525ba
pazmiguela <mpmrhm2018@gmail.com>
2025-07-13 17:20:05 +0800

    20250713

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Test for Swag Labs', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/v1/');
   await expect(page.locator('[data-test="username"]')).toBeEmpty();
   await expect(page.locator('#login_button_container')).toMatchAriaSnapshot(`
