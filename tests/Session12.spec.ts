b39d7962b8d3e4447e57fcc18bdbfd355316fd47
pazmiguela <mpmrhm2018@gmail.com>
2025-07-26 17:19:29 +0800

    20250726

--- tests/Session12.spec.ts
+++ tests/Session12.spec.ts
@@ -1,1 +1,35 @@
 /* Apply test hooks and test grouping and other session 12 topics */
+
+import { test, expect } from '@playwright/test';
+
+test.describe('Session 12 Tests', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://thelab.boozang.com/');
+    await expect(page.getByText('theLab')).toBeVisible();
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.screenshot({ path: 'test-results/screenshots/${test.info().title}.png', fullPage: true });
+  });
+
+test('Come back from Collecting kittens', async ({ page }) => {
+  await page.getByRole('button', { name: 'Menu' }).click();
+  await page.getByRole('link', { name: 'Collecting kittens' }).click();
+  await expect(page.getByRole('heading', { name: 'Collecting Kittens', exact: true })).toBeVisible();
+});
+
+
+test('Come back from Canvas game', async ({ page }) => {
+  await page.getByRole('button', { name: 'Menu' }).click();
+  await page.getByRole('link', { name: 'Canvas Game' }).click();
+  await expect(page.getByRole('heading', { name: 'Canvas Game', exact: true })).toBeVisible();
+});
+
+test('Come back from Concat strings', async ({ page }) => {
+  await page.getByRole('button', { name: 'Menu' }).click();
+  await page.getByRole('link', { name: 'Concat strings' }).click();
+  await expect(page.getByRole('heading', { name: 'Concatenate Strings', exact: true })).toBeVisible();
+});
+
+});
