187d6b668f23d408cd34fce55fe7a74106dfaed0
pazmiguela <mpmrhm2018@gmail.com>
2025-07-26 17:39:15 +0800

    20250726

--- tests/Session13.spec.ts
+++ tests/Session13.spec.ts
@@ -1,1 +1,7 @@
-/*Apply Auth Setup + Storage State to spec files related to Sauce Demo*/
+/*Apply Auth Setup + Storage State to spec files related to Sauce Demo
+
+Hi Sir Reg! Please see my other repository @
+
+https://github.com/pazmiguela/STaA2
+
+*/
--- tests/Session14.spec.ts
+++ tests/Session14.spec.ts
@@ -1,2 +1,8 @@
 /* Apply POM in auth setup and
-Apply POM + Fixtures for login.spec.ts or .js*/
+Apply POM + Fixtures for login.spec.ts or .js
+
+Hi Sir Reg! Please see my other rep
+
+https://github.com/pazmiguela/STaA2
+
+*/
